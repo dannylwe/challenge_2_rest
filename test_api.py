d3ac6f883fbbe4dc0fabfa8c7168aedb92e8a6db
Daniel Lwetabe <dannylwe@gmail.com>
2018-10-19 21:06:43 +0300

    Fixed Unite tests

--- test_api.py
+++ test_api.py
@@ -1,37 +1,42 @@
 from unittest import TestCase
-import requests
+import storemanager_api
 
 class Test(TestCase):
 
-	def test_sanity(self):
-		response = requests.get('http://localhost:5000/hello')
+	def setUp(self):
+		self.app = storemanager_api.app.test_client() 
+
+	def test_hello_world(self):
+		response = self.app.get('/hello')
+		self.assertEqual(response.status_code, 200)
+	
+	def test_get_all_products(self):
+		response = self.app.get('/api/v1/all_products')
 		self.assertEqual(response.status_code, 200)
 
-	def test_sanity_1(self):
-		response = requests.get('http://localhost:5000/hello')
-		self.assertEqual(response.ok, True)
-
-	def test_get_all_products(self):
-		response = requests.get('http://localhost:5000/api/v1/all_products')
-		self.assertEqual(response.ok, True)
-
 	def test_attendants(self):
-		response = requests.get('http://localhost:5000/api/v1/attendants')
-		self.assertEqual(response.ok, True)
+		response = self.app.get('/api/v1/attendants')
+		self.assertEqual(response.status_code, 200)
 
 	def test_sale_order(self):
-		response = requests.get('http://localhost:5000/api/v1/g/sale_order/')
-		self.assertEqual(response.ok, True)
+		response = self.app.get('/api/v1/g/sale_order/')
+		self.assertEqual(response.status_code, 200)
 
 	def test_get_one_products(self):
-		response = requests.get('http://localhost:5000/api/v1/one_product/1')
-		self.assertEqual(response.ok, True)
+		response = self.app.get('/api/v1/one_product/1')
+		self.assertEqual(response.status_code, 200)
 
 	def test_delete_one_attendants(self):
-		response = requests.delete('http://localhost:5000/api/v1/del_one_attendant/1')
-		self.assertEqual(response.ok, True)
+		response = self.app.delete('/api/v1/del_one_attendant/1')
+		self.assertEqual(response.status_code, 200)
 
 	def test_delete_one_product(self):
-		response = requests.delete('http://localhost:5000/api/v1/del_one_product/1')
-		self.assertEqual(response.ok, True)
+		response = self.app.delete('/api/v1/del_one_product/1')
+		self.assertEqual(response.status_code, 200)
+	
+
+	
+
+    
+        
 
