5ca73e831053f0f1336b4dde755a7b97d70c2817
Daniel Lwetabe <dannylwe@gmail.com>
2018-10-25 11:07:08 +0300

    chore(travis, coveralls): build fix

---
+++ test_api.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+import app
+
+class Test(TestCase):
+
+	def setUp(self):
+		#self.app = storemanager_api.app.test_client()
+		self.app = app.app.test_client()  
+
+	def test_hello_world(self):
+		response = self.app.get('/hello')
+		self.assertEqual(response.status_code, 200)
+	
+	def test_get_all_products(self):
+		response = self.app.get('/api/v1/all_products')
+		self.assertEqual(response.status_code, 200)
+
+	def test_attendants(self):
+		response = self.app.get('/api/v1/attendants')
+		self.assertEqual(response.status_code, 200)
+
+	def test_sale_order(self):
+		response = self.app.get('/api/v1/g/sale_order/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_get_one_products(self):
+		response = self.app.get('/api/v1/one_product/1')
+		self.assertEqual(response.status_code, 200)
+
+	def test_delete_one_attendants(self):
+		response = self.app.delete('/api/v1/del_one_attendant/1')
+		self.assertEqual(response.status_code, 204)
+
+	def test_delete_one_product(self):
+		response = self.app.delete('/api/v1/del_one_product/1')
+		self.assertEqual(response.status_code, 204)
+	
+
+	
+
+    
+        
+
