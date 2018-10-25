c2494e63a90d9483e4113c89772a3768a9688968
Daniel Lwetabe <dannylwe@gmail.com>
2018-10-25 08:42:08 +0300

    chore(refactor): code refactor to MVC convention

--- app.py
+++ app.py
@@ -1,89 +1,6 @@
-from flask import Flask
-from flask_restplus import Api, Resource, fields
-
-app = Flask(__name__)
-api = Api(app, version='1.1', title='Store Manager Api', 
-	description='An Api that supplies data to the store manager app')
+from models import *
 
 base_url= "/api/v1"
-
-attendant = [
-	{
-	"id": 1,
-	"name": "Tony Stark",
-	"email": "tony@gmail.com",
-	"password": "iamironman",
-	"contact": "1800-TONY-STARK",
-	},
-
-	{
-	"id": 2,
-	"name": "Peter Parker",
-	"email": "peter@gmail.com",
-	"password": "auntmay",
-	"contact": "0758123456",
-	},
-
-	{
-	"id": 3,
-	"name": "Hulk",
-	"email": "hulk@gmail.com",
-	"password": "banner",
-	"contact": None,
-	},
-]
-
-products = [
-	{
-	"id": 0, "name": None, "quantity": None
-	},
-
-	{
-	"id": 1, "name": "Kitchen towels", "quantity": 15, "price": 4000
-	},
-
-	{
-	"id": 2, "name": "blue band", "quantity": 5, "price": 1000
-	},
-]
-
-"""should use OOP to create these"""
-
-sale_order = [
-	{
-	"order_id": 123456,
-	"name": ['Kitchen towels', 'blue band'],
-	"quantity": 3,
-	"price": 6000,
-	"attendant": "Tony Stark",
-	"attendant_id": 1,
-	"category": "Households"
-	},
-]
-
-#Products.append(products)
-
-"""for i in attendant:
-	print(i['id'])
-"""
-#print(products[0])
-
-a_product = api.model('product', {
-	"id": fields.Integer('Id of item'),
-	"name": fields.String('name of product'), 
-	"quantity": fields.Integer('Number of products'),
-	"price": fields.Integer('Retail price'),
-	})
-
-a_sale_order = api.model('sale_order', {
-	"order_id": fields.Integer('Unique order Id'),
-	"name": fields.String('Name of product(s)'),
-	"quantity": fields.Integer('Numerical quantity of products'),
-	"price": fields.Integer('Cumulative price'),
-	"attendant": fields.String('Name of attendant'),
-	"attendant_id": fields.String('Unique Id of attendant'),
-	"category": fields.String('category of product if any'),
-	})
 
 @api.route('/hello')
 class HelloWorld(Resource):
@@ -125,7 +42,7 @@ class One_product(Resource):
 
 	def get(self, id):
 		"Get one product by Id"
-			
+
 		return {"product": products[id]}, 200
 
 #@api.response(204, 'successfully deleted.')
@@ -175,6 +92,3 @@ class Sale_order_post(Resource):
 		new_sale_order = api.payload
 		sale_order.append(new_sale_order)
 		return {"successfully added new sale order": new_sale_order}, 201
-	
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,85 @@
+from flask_restplus import Api, Resource, fields
+from flask import Flask
+
+app = Flask(__name__)
+
+api = Api(app, version='1.1', title='Store Manager Api', 
+	description='An Api that supplies data to the store manager app')
+
+attendant = [
+	{
+	"id": 1,
+	"name": "Tony Stark",
+	"email": "tony@gmail.com",
+	"password": "iamironman",
+	"contact": "1800-TONY-STARK",
+	},
+
+	{
+	"id": 2,
+	"name": "Peter Parker",
+	"email": "peter@gmail.com",
+	"password": "auntmay",
+	"contact": "0758123456",
+	},
+
+	{
+	"id": 3,
+	"name": "Hulk",
+	"email": "hulk@gmail.com",
+	"password": "banner",
+	"contact": None,
+	},
+]
+
+products = [
+	{
+	"id": 0, "name": None, "quantity": None
+	},
+
+	{
+	"id": 1, "name": "Kitchen towels", "quantity": 15, "price": 4000
+	},
+
+	{
+	"id": 2, "name": "blue band", "quantity": 5, "price": 1000
+	},
+]
+
+"""should use OOP to create these"""
+
+sale_order = [
+	{
+	"order_id": 123456,
+	"name": ['Kitchen towels', 'blue band'],
+	"quantity": 3,
+	"price": 6000,
+	"attendant": "Tony Stark",
+	"attendant_id": 1,
+	"category": "Households"
+	},
+]
+
+#Products.append(products)
+
+"""for i in attendant:
+	print(i['id'])
+"""
+#print(products[0])
+
+a_product = api.model('product', {
+	"id": fields.Integer('Id of item'),
+	"name": fields.String('name of product'), 
+	"quantity": fields.Integer('Number of products'),
+	"price": fields.Integer('Retail price'),
+	})
+
+a_sale_order = api.model('sale_order', {
+	"order_id": fields.Integer('Unique order Id'),
+	"name": fields.String('Name of product(s)'),
+	"quantity": fields.Integer('Numerical quantity of products'),
+	"price": fields.Integer('Cumulative price'),
+	"attendant": fields.String('Name of attendant'),
+	"attendant_id": fields.String('Unique Id of attendant'),
+	"category": fields.String('category of product if any'),
+	})
